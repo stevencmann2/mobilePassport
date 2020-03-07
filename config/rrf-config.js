0df36956fe8c2f0c34ddc669eb609ad4bffae07b
An Phan <antphan98@gmail.com>
2020-03-07 13:43:37 -0600

    created and set up firebase/redux folders

---
+++ config/rrf-config.js
@@ -0,0 +1,8 @@
+const rrfConfig = {
+    userProfile: 'users',
+    useFirestoreForProfile: true,
+    // profileParamsToPopulate: [
+    //     { child: 'role', root: 'roles' }, // populates user's role with matching role object from roles
+    // ],
+};
+export default rrfConfig;
