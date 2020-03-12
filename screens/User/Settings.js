8b6569f3cca50abb6e3f93a151d18b6822d88e1c
An Phan <antphan98@gmail.com>
2020-03-11 19:17:13 -0500

    put mock data for Victory into settings.js

--- screens/User/Settings.js
+++ screens/User/Settings.js
@@ -4,6 +4,8 @@ import {
     View, 
     StyleSheet
 } from 'react-native'
+// import { StyleSheet, View } from "react-native";
+import { VictoryBar, VictoryChart, VictoryTheme } from "victory-native";
 
 
 
@@ -11,27 +13,46 @@ import {
 const Settings = props =>{
     
     const { navigation } = props
-
+    const data = [
+        { quarter: 1, earnings: 13000 },
+        { quarter: 2, earnings: 16500 },
+        { quarter: 3, earnings: 14250 },
+        { quarter: 4, earnings: 19000 }
+      ];
+      
     
     return(
-        <View style={styles.screen}>
+        <View style={styles.container}>
             <View>
                 <Text style={{color: 'black'}}>
                         USER SETTINGS HOMEPAGE
+                        <Text>
+                            hello
+                        </Text>
                 </Text>
             </View>
+            <Text>
+                hello
+            </Text>
+            
+            {/* <View style={styles.container}>
+        <VictoryChart width={350} theme={VictoryTheme.material}>
+          <VictoryBar data={data} x="quarter" y="earnings" />
+        </VictoryChart>
+      </View> */}
+
         </View>
     )
 }
 
-const styles = StyleSheet.create({
-    screen: {
-      flex: 1,
-      alignItems: 'center',
-      justifyContent: 'center',
-      marginTop: 40,
-    }
-    
-})
 
-export default Settings
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: "center",
+    alignItems: "center",
+    backgroundColor: "#f5fcff"
+  }
+});
+
+export default Settings;
