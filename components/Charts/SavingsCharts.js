a1b0e54b9b2b5e686fa8deb75d04a85d94749543
Steven Mann <stevencmann2@gmail.com>
2020-03-16 13:31:04 -0500

    working on structuring data for savings charts

--- components/Charts/SavingsCharts.js
+++ components/Charts/SavingsCharts.js
@@ -10,7 +10,6 @@ const SavingsCharts = () =>{
     const firestore = useFirestore();
     const selectedTrip = useSelector(state=> state.tripID.id)
 
-
     useFirestoreConnect([
         { collection: 'Trips', doc: `${selectedTrip}`},
         { collection: 'Trips', 
@@ -66,7 +65,7 @@ const SavingsCharts = () =>{
             storeAs: 'AirfareSavings'
         },
    ]);
-
+   // REDUX STORE
    const BudgetData = useSelector(state =>state.firestore.data.BudgetBreakdownData)
    const fullStoreSavingsArr = useSelector(state=> state.firestore.ordered.SavingsData)
         const EmergencySavingsArr = useSelector(state=> state.firestore.ordered.EmergencySavings)
@@ -76,7 +75,10 @@ const SavingsCharts = () =>{
         const FoodDrinkSavingsArr = useSelector(state=> state.firestore.ordered.FoodDrinkSavings)
         const LodgingSavingsArr = useSelector(state=> state.firestore.ordered.LodgingSavings)
         const AirfareSavingsArr = useSelector(state=> state.firestore.ordered.AirfareSavings)
- 
+        console.log("==============================")
+        console.log(BudgetData[selectedTrip])
+        console.log("==============================")
+   // NEW ARRAY OF ONLY AMOUNTS DONE BY CATEGORY
    const FoodValArr = _.map(FoodDrinkSavingsArr, 'Amount')
    const EmergencyValArr = _.map(EmergencySavingsArr, 'Amount')
    const MiscValArr = _.map(MiscSavingsArr, 'Amount')
@@ -85,6 +87,7 @@ const SavingsCharts = () =>{
    const LodgingValArr = _.map(LodgingSavingsArr, 'Amount')
    const AirfareValArr = _.map(AirfareSavingsArr, 'Amount')
 
+   //TOTAL ABOVE ARRAYS
    const Foodtotal = _.sum(FoodValArr)
    const Emergencytotal = _.sum(EmergencyValArr)
    const Misctotal = _.sum(MiscValArr)
@@ -93,25 +96,54 @@ const SavingsCharts = () =>{
    const Lodgingtotal = _.sum(LodgingValArr)
    const Airfaretotal = _.sum(AirfareValArr)
 
+   // BUDGET BREAKDOWN BY CATEGORY
+   const FoodBudget = _.get(BudgetData[selectedTrip], 'Food & Drink')
+   const EmergencyBudget = _.get(BudgetData[selectedTrip], 'Emergency')
+   const MiscBudget = _.get(BudgetData[selectedTrip], 'Misc')
+   const ActivitiesBudget = _.get(BudgetData[selectedTrip], 'Activities')
+   const TransportationBudget = _.get(BudgetData[selectedTrip], 'Tranportation')
+   const LodgingBudget = _.get(BudgetData[selectedTrip], 'Lodging')
+   const AirfareBudget = _.get(BudgetData[selectedTrip], 'Airfare')
 
-   console.group("Savings Sum Arrays")
-   console.log("Savings Store",fullStoreSavingsArr)
-   console.log("Budget Data", BudgetData)
-   console.log("Emergency", Emergencytotal)
-   console.log("Misc", Misctotal)
-   console.log("Act", Activitiestotal)
-   console.log("Trans", Transportationtotal)
-   console.log("Food", Foodtotal)
-   console.log("hotel", Lodgingtotal)
-   console.log("Air", Airfaretotal)
+   console.group()
+   console.log(FoodBudget, "food")
+   console.log(EmergencyBudget, "emerg")
+   console.log(MiscBudget, "misc")
+   console.log(ActivitiesBudget, "act")
+   console.log(TransportationBudget, "transpo")
+   console.log(LodgingBudget, "lodge")
+   console.log(AirfareBudget, "airfare")
    console.groupEnd()
-
 
    const expenseTotal = 3220
    const CategoryTotal = 5000
    const ExpPercent  = (expenseTotal/CategoryTotal)
    const PercentasText = (ExpPercent.toFixed(2))*100
    console.log(ExpPercent)
+
+   //Make Arrays of Data
+   // RESULT IS y:NaN IF UNDEFINED
+
+    const FoodData = [{x: 1, y:(Foodtotal/FoodBudget)},{x:2, y:1-(Foodtotal/FoodBudget)}]
+    const EmergencyData = [{x:1, y:(Emergencytotal/EmergencyBudget)}, {x:2, y:1-(Emergencytotal/EmergencyBudget)}]
+    const MiscData = [{x:1, y:(Misctotal/MiscBudget)}, {x:2, y:1-(Misctotal/MiscBudget)}]
+    const ActivitiesData = [{x:1, y:(Activitiestotal/ActivitiesBudget)}, {x:2, y:1-(Activitiestotal/ActivitiesBudget)}]
+    const TranportationData = [{x:1, y:(Transportationtotal/TransportationBudget)}, {x:2, y:1-(Transportationtotal/TransportationBudget)}]
+    const LodgingData = [{x:1, y:(Lodgingtotal/LodgingBudget)}, {x:2, y:1-(Lodgingtotal/LodgingBudget)}]
+    const AirfareData = [{x:1, y:(Airfaretotal/AirfareBudget)}, {x:2, y:1-(Airfaretotal/AirfareBudget)}]
+    
+    console.group()
+    console.log(FoodData, "food obj")
+    console.log(EmergencyData, "emerg obj")
+    console.log(MiscData, "Miscobj")
+    console.log(ActivitiesData, "Activities obj")
+    console.log(TranportationData, "Transport obj")
+    console.log(LodgingData, "Lodging obj")
+    console.log(AirfareData, "Air obj")
+    console.groupEnd()
+
+
+
 
 
 if(!isLoaded(fullStoreSavingsArr)){
@@ -121,8 +153,7 @@ if(!isLoaded(fullStoreSavingsArr)){
                 <ActivityIndicator  
                     size="large"
                 /> 
-            </View>
-            
+            </View>    
             )
     }
 
@@ -131,7 +162,7 @@ return(
     <View style={styles.screen}>
         <View style={styles.chartContainer}>
             <VictoryPie 
-                data={[{ x: 1, y: ExpPercent}, {x: 2, y: (1-ExpPercent)} ]}  
+                data={LodgingData}  
                 width={200}  
                 innerRadius={40}
                 labels={() => null}
@@ -146,6 +177,7 @@ return(
                 }}
             /> 
         </View>
+        
     </View>
 
 )
@@ -165,6 +197,20 @@ return(
 // data={[{ name: PercentasText }]}
 // // orientation="horizontal"
 // />
+
+
+
+//    console.group("Savings Sum Arrays")
+//    console.log("Savings Store",fullStoreSavingsArr)
+//    console.log("Budget Data", BudgetData)
+//    console.log("Emergency", Emergencytotal)
+//    console.log("Misc", Misctotal)
+//    console.log("Act", Activitiestotal)
+//    console.log("Trans", Transportationtotal)
+//    console.log("Food", Foodtotal)
+//    console.log("hotel", Lodgingtotal)
+//    console.log("Air", Airfaretotal)
+//    console.groupEnd()
   
 
    }
