b1808ce1cd8f92ff1a71923681fc98d2a816dcaa
An Phan <antphan98@gmail.com>
2020-03-19 20:53:33 -0500

    readded event mods

--- screens/User/TripsView/SingleUser/Itinerary.js
+++ screens/User/TripsView/SingleUser/Itinerary.js
@@ -1,13 +1,7 @@
-<<<<<<< HEAD
-import React, {Component} from 'react';
-import {Alert, StyleSheet, Text, View, SafeAreaView, TouchableOpacity} from 'react-native';
-import {Avatar, Button} from 'react-native-elements'
-=======
 import React, {useState} from 'react';
 import {Alert, StyleSheet, Text, View, 
          SafeAreaView, ActivityIndicator,Keyboard, Switch} from 'react-native';
-import {Icon, Overlay, Button } from 'react-native-elements'
->>>>>>> ace7846d16fd004aedef21bf578a3d97a00a1599
+import {Avatar, Icon, Overlay, Button } from 'react-native-elements'
 import Input from '../../../../components/Input';
 import {Agenda} from 'react-native-calendars';
 import moment from 'moment';
@@ -127,49 +121,6 @@ if(HoursTest && MinTest && titleText.length > 0 && descriptionText.length > 0
                     date: dateString
          }]
         }
-<<<<<<< HEAD
-      ]
-        }}
-        renderItem={item => (
-          <View style={[styles.item]}>
-            <Text
-              style={{ color: '#6a6a6a'}}
-            >
-              {item.time} 
-            </Text>
-            <Text style={{fontWeight: "bold", marginTop: 5, fontSize: 15}}
-            >
-                {item.title}
-            </Text>
-            <Avatar
-  size="medium"
-  rounded
-  title="MT"
-  onPress={() => console.log("Works!")}
-  activeOpacity={0.7}
-  containerStyle={{flex: 2, marginLeft: 260, marginBottom: 20}}
-/>
-          </View>
-        )}
-        renderEmptyData={() => <View style={{ flex: 1, justifyContent: 'center', alignContent: 'center' }}><Text>No appointments</Text></View>}
-    markedDates={{
-      '2020-05-16': {selected: true, marked: true},
-      '2020-05-17': {marked: true},
-      '2020-05-18': {disabled: true}
-    }}
-    renderEmptyData={() => <View style={{ flex: 1, justifyContent: 'center', alignContent: 'center' }}><Text>No appointments</Text></View>}
-    rowHasChanged={(r1, r2) => r1.title !== r2.title}
-        theme={{
-          agendaDayNumColor: 'rgba(190,82,2, 0.75)',
-          agendaDayTextColor: 'rgba(190,82,2, 0.75)',
-          agendaKnobColor: '#efefef',
-          agendaTodayColor: 'rgba(190,82,2, 0.75)',
-          dotColor: 'rgba(190,82,2, 0.75)',
-          todayTextColor: 'rgba(190,82,2, 0.75)',
-          selectedDayBackgroundColor: 'rgba(190,82,2, 0.75)',
-          'stylesheet.calendar.header': {
-            week: { marginTop: 0, flexDirection: 'row', justifyContent: 'space-between' }
-=======
   
    
       try{    
@@ -221,7 +172,6 @@ const toggleAlert = () => {
       [
           {text: 'Ok',
           onPress: ()=>console.log('Ok Pressed, Alert Closed')
->>>>>>> ace7846d16fd004aedef21bf578a3d97a00a1599
           }
       ]
 )
@@ -396,9 +346,21 @@ return(
                     <Text
                       style={{ color: '#6a6a6a' }}
                     >
-                      {item.time} &mdash; {item.title}
-                    </Text>
-                  </View>
+                      {item.time} 
+                      {item.time} 
+            </Text>
+            <Text style={{fontWeight: "bold", marginTop: 5, fontSize: 15}}
+            >
+                {item.title}
+            </Text>
+            <Avatar
+  size="medium"
+  rounded
+  title="MT"
+  onPress={() => console.log("Works!")}
+  activeOpacity={0.7}
+  containerStyle={{flex: 2, marginLeft: 260, marginBottom: 20}}
+/>
                 )
               }
                 // renderEmptyData={() => <View style={{ flex: 1, justifyContent: 'center', alignContent: 'center' }}><Text>No appointments</Text></View>}
